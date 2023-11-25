547d5d0bff2edf8da4e551e3123a5ab4aac967fb
Jaiganesh-MJ <63336185+Jaiganesh-MJ@users.noreply.github.com>
2023-11-25 13:13:44 +0530

    Create mariadb.sh

---
+++ mariadb.sh
@@ -0,0 +1,9 @@
+sudo apt update
+
+sudo apt-get install wget software-properties-common dirmngr ca-certificates apt-transport-https -y
+
+sudo apt install mariadb-server mariadb-client
+
+mariadb --version
+
+sudo mysql_secure_installation
